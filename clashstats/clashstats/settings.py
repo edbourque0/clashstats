2ad22ccffaca10b9696f7c265bfc857ef51bdd88
Edouard Bourque <edbourque0@gmail.com>
2024-09-04 17:14:30 -0400

    Added WSGI app

--- clashstats/clashstats/settings.py
+++ clashstats/clashstats/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
+WSGI_APPLICATION = 'clashstats.wsgi.application'
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -120,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
