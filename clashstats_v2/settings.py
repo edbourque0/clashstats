ed9998895c78734560ae62e5ab9ec51f9961ebc5
GitHub <edbourque0@gmail.com>
2025-11-18 19:37:07 -0500

    Improve environment handling in cron setup and update refresh cron schedule configuration

--- clashstats_v2/settings.py
+++ clashstats_v2/settings.py
@@ -127,8 +127,9 @@ STATIC_URL = 'static/'
 API_KEY = os.getenv("CLASH_API_KEY")
 
 CRONJOBS = [
-    ('*/1 * * * *', 'django.core.management.call_command', ['refresh_clan']),
+    (os.getenv('REFRESH_CRON', '* */6 * * *'), 'django.core.management.call_command', ['refresh_clan']),
 ]
 
 CRONTAB_LOCK_JOBS = True
 CRONTAB_COMMAND_SUFFIX = '>> /var/log/django-cron.log 2>&1'
+CRONTAB_COMMAND_PREFIX = '. /etc/environment;'
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,9 +16,7 @@ echo "Starting cron daemon..."
 cron
 
 echo "Adding env var to crontab..."
-printenv > env.txt
-cat /var/spool/cron/crontabs/root >> env.txt
-cat env.txt > /var/spool/cron/crontabs/root
+printenv | grep -v "no_proxy" >> /etc/environment
 
 echo "Starting Gunicorn..."
 exec gunicorn clashstats_v2.wsgi:application --bind 0.0.0.0:8000 --workers 4
